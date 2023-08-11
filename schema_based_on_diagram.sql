0415d6eb2d7b67b5e6bbfff70314d9a42673e2ac
Daniel Ochuba Ugochukwu <danielochuba78@gmail.com>
2023-08-11 12:24:29 +0100

    create tables in schema_based_on_diagram.sql file

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,1 +1,21 @@
+CREATE DATABASE clinic;
+
+CREATE TABLE patients (
+id SERIAL PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+date_of_birth DATE NOT NULL
+);
+
+CREATE TABLE treatments (
+  id SERIAL PRIMARY KEY,
+  type VARCHAR(200) NOT NULL,
+  name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE medical_histories (
+  id SERIAL PRIMARY KEY,
+  admitted_at TIMESTAMP NOT NULL,
+  patient_id INT REFERENCES patients(id),
+  status VARCHR(100) NOT NULL
+  );
 
