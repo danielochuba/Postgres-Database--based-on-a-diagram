9d205f5edafa0214f4beba4348fe3fa433aa8e5a
Sebastian Hernández <sebastian4hm@gmail.com>
2023-08-11 08:36:26 -0600

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -16,6 +16,21 @@ CREATE TABLE medical_histories (
   id SERIAL PRIMARY KEY,
   admitted_at TIMESTAMP NOT NULL,
   patient_id INT REFERENCES patients(id),
-  status VARCHR(100) NOT NULL
+  status VARCHAR(100) NOT NULL
   );
 
+CREATE TABLE invoices (
+  id SERIAL PRIMARY KEY,
+  total_amount DECIMAL(18,2) NOT NULL,
+  generated_at TIMESTAMP NOT NULL,
+  payed_at TIMESTAMP NOT NULL,
+  medical_history_id INT REFERENCES medical_histories(id)
+  );
+CREATE TABLE invoice_items (
+  id SERIAL PRIMARY KEY,
+  unit_price DECIMAL(18,2) NOT NULL,
+  quantity INT NOT NULL,
+  total_price DECIMAL(20,2) NOT NULL,
+  invoice_id INT REFERENCES invoices(id),
+  treatment_id INT REFERENCES treatments(id)
+  );
